c46276a834134ee2d9bbca0d776e9508b9aa66ef
Dzhokha <67861550+Dzhokha@users.noreply.github.com>
2022-12-09 18:25:01 +0000

    added whose turn is it (to track whose turn is it)
    it is infinetly recursing because of where the new grid is placed it just copies each new grid individually

--- recursive main.py
+++ recursive main.py
@@ -2,16 +2,18 @@ import random
 import copy
 
 beads = 8
+WHOSE_TURN_IS_IT = 1  # will be passed to object. if it is divisble by 2 then it is players turn
+GRIDS = [[8, 8, 0], [0, 8, 0], [0, 0, 0]]
 
-GRIDS = [[8,8,0],[0,8,0],[0,0,0]]
 
 class Matchbox:
-    def __init__(self, grid, parentnodes, childnodes, beads):
+    def __init__(self, grid, parentnodes, childnodes, beads, WhoseTurn):
         self.grid = grid
         self.parentnode = parentnodes
         self.childnodes = childnodes
-        self.create_child()
         self.beads = beads
+        self.WhoseTurn = WhoseTurn
+        self.creating_child()
 
     def place_beads(self):
         for i in range(1, 3):
@@ -45,14 +47,24 @@ class Matchbox:
             return True
 
     def creating_child(self):
-        new_grid = copy.deepcopy(self.grid)
-        for i in range(3):
-            for j in range(3):
-                if self.grid[i][j] != "x" and self.grid[i][j] != "o":
-                    new_grid[i][j] = "o"
-                    self.childnodes.append(Matchbox(new_grid, [self], None, self.beads))
-                if self.grid[i][j] != "x" and self.grid[i][j] != "o":
-                    new_grid[i][j] = "x"
-                    self.childnodes.append(Matchbox(new_grid, [self], None, self.beads))
+        if self.WhoseTurn % 2 != 0:
+            self.WhoseTurn += 1
+            for i in range(3):
+                for j in range(3):
+                    new_grid = copy.deepcopy(self.grid)
+                    if new_grid[i][j] != "x" and new_grid[i][j] != "o":
+                        new_grid[i][j] = "x"
+                        self.childnodes.append(Matchbox(new_grid, [self], [], self.beads, self.WhoseTurn))
+        elif self.WhoseTurn % 2 == 0:
+            self.WhoseTurn += 1
+            for i in range(3):
+                for j in range(3):
+                    new_grid = copy.deepcopy(self.grid)
+                    if self.grid[i][j] != "x" and self.grid[i][j] != "o":
+                        new_grid[i][j] = "o"
+                        self.childnodes.append(Matchbox(new_grid, [self], [], self.beads, self.WhoseTurn))
 
 
+matchbox = Matchbox(GRIDS, [], [], beads, WhoseTurn=WHOSE_TURN_IS_IT)
+
+print(matchbox.childnodes[0].grid)
