1e0c1a05a9f595f171aa1797fe698786b4089c24
Dzhokha <67861550+Dzhokha@users.noreply.github.com>
2023-02-13 22:24:17 +0000

    Solved the generation problem

--- recursive main.py
+++ recursive main.py
@@ -5,7 +5,6 @@ beads = 8
 WHOSE_TURN_IS_IT = 1  # will be passed to object. if it is divisible by 2 then it is players turn
 GRIDS = [[8, 8, 8], [8, 8, 8], [8, 8, 8]]
 
-
 # grid is given as a 2d array. First layer of arrays are rows. 2nd layer is columns
 
 class Matchbox:
@@ -100,8 +99,9 @@ class Matchbox:
         """
         for i in range(3):
             for j in range(3):
-                if self.grid[i][j] != self.beads and self.grid[i][j] != 'x' and self.grid[i][j] != 'x':
+                if self.grid[i][j] != self.beads and self.grid[i][j] != 'x' and self.grid[i][j] != 'o':
                     self.grid[i][j] = self.beads
+        return self.grid
 
     def creating_child(self, beads):
         """
@@ -112,24 +112,26 @@ class Matchbox:
         """
         thingy = copy.deepcopy(self.WhoseTurn)    # todo deelte this
         thingy += 1
-        if self.WhoseTurn % 2 != 0:
+        if (self.WhoseTurn % 2) != 0:
             for i in range(3):
                 for j in range(3):
                     new_grid = copy.deepcopy(self.grid)
-                    if new_grid[i][j] != "x" and new_grid[i][j] != "o" and new_grid[i][
-                        j] != 0:  # should never be zero unless user specifically defines 0 to be from the start
+                    if new_grid[i][j] != "x" and new_grid[i][j] != "o" and new_grid[i][j] != 0:  # should never be zero unless user specifically defines 0 to be from the start
                         new_grid[i][j] = "x"
                         self.childnodes.append(Matchbox(new_grid, [self], [], self.beads, thingy)) #TODO change thingy to WhoseTurn + 1
                     else:
                         self.childnodes.append(None)
-        elif self.WhoseTurn % 2 == 0:
+        elif (self.WhoseTurn % 2) == 0:
             for i in range(3):
                 for j in range(3):
                     new_grid = copy.deepcopy(self.grid)
                     if self.grid[i][j] != "x" and self.grid[i][j] != "o":
                         new_grid[i][j] = "o"
+                        # print(f"flag one 1\n\nnew grid is {new_grid}")
                         self.childnodes.append(Matchbox(new_grid, [self], [], self.beads, thingy)) #TODO change thingy to WhoseTurn + 1
+
                     else:
+                        # print(f"flag 2\n")
                         self.childnodes.append(None)
 
     def user_go(self, grid):
@@ -157,11 +159,13 @@ class Matchbox:
                 for col in range(0, 3):
                     if self.grid[row][col] != 'x' and self.grid[row][col] != 'o':
                         count += self.grid[row][col]      #count the total number of beads
+
             random_x = random.randint(0, count)       #set the random number
             break_out_flag = False
             index = 0           #set the index of the game.
             for row2 in range(0, 3):
                 for col2 in range(0, 3):
+                    print(f"random x is {random_x}")
                     if type(self.grid[row2][col2]) == type(0):  # checking whether column in a row is an integer
                         if random_x - self.grid[row2][col2] <= 0:
                             break_out_flag = True
@@ -171,13 +175,16 @@ class Matchbox:
                             random_x -= self.grid[row2][col2]
                 if break_out_flag:
                     break
+                    # else:
+                    #     index += 1
+                    #     random_x -= self.grid[row2][col2]
             placed_x = (row2, col2)
-            print(f"here we go...\nINEDX is {index},\nCURRENT game board is {self.grid}, \nCHILDNODES are {self.childnodes}")
+            print(f"here we go...\nINDEX is {index},\nCURRENT game board is {self.grid}, \nCHILDNODES are {self.childnodes}")
             self.result = self.childnodes[index].game()
         else:
             user_cords = self.user_go(self.grid)
             index = ((3 * user_cords[0]) + (user_cords[1])) # calculates the index of the matchbox with the same game
-                                                            # grid inside the childnode list.
+            # grid inside the childnode list.
             self.result = self.childnodes[index].game()
         if self.checking_win()[1] == "Menace":
             return True
@@ -191,8 +198,8 @@ class Matchbox:
             #     pass
 
 
-matchbox = Matchbox(GRIDS, [], [], beads, WhoseTurn=WHOSE_TURN_IS_IT)
+# matchbox = Matchbox(GRIDS, [], [], beads, WhoseTurn=WHOSE_TURN_IS_IT)
 
+matchbox = Matchbox(GRIDS, [], [], beads, WhoseTurn=WHOSE_TURN_IS_IT)
 matchbox.game()
-print(matchbox.grid)
-print(matchbox.childnodes[0])
+# print(f"\n\nParent node is {matchbox.childnodes[0].grid}, Whoseturn {matchbox.childnodes[0].WhoseTurn}\n childnode is {matchbox.childnodes[0].childnodes[2].grid}\n\n")
