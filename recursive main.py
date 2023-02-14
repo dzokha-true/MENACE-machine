f6f08df32fe92e310427b76b40b9da1e1c34d618
Dzhokha <67861550+Dzhokha@users.noreply.github.com>
2023-02-14 20:35:28 +0000

    fixed the game just addind finishiing touches

--- recursive main.py
+++ recursive main.py
@@ -4,7 +4,6 @@ import copy
 beads = 8
 WHOSE_TURN_IS_IT = 1  # will be passed to object. if it is divisible by 2 then it is players turn
 GRIDS = [[8, 8, 8], [8, 8, 8], [8, 8, 8]]
-
 # grid is given as a 2d array. First layer of arrays are rows. 2nd layer is columns
 
 class Matchbox:
@@ -17,13 +16,13 @@ class Matchbox:
         beads: number of beads in the matchbox
         WhoseTurn: determines whose turn it is to play (1 for player, 0 for Menace)
         """
-
         self.grid = grid
         self.parentnode = parentnodes
         self.childnodes = childnodes
         self.beads = beads
         self.WhoseTurn = WhoseTurn
         self.next_step()
+        self.placed_x = 0
 
     def next_step(self):
         """
@@ -90,7 +89,7 @@ class Matchbox:
             else:
                 return [True, "Player"]
         else:
-            return [False, 0]
+            return [False, "draw"]
 
     def placing_beads(self):
         """
@@ -118,7 +117,7 @@ class Matchbox:
                     new_grid = copy.deepcopy(self.grid)
                     if new_grid[i][j] != "x" and new_grid[i][j] != "o" and new_grid[i][j] != 0:  # should never be zero unless user specifically defines 0 to be from the start
                         new_grid[i][j] = "x"
-                        self.childnodes.append(Matchbox(new_grid, [self], [], self.beads, thingy)) #TODO change thingy to WhoseTurn + 1
+                        self.childnodes.append(Matchbox(new_grid, self, [], self.beads, thingy)) #TODO change thingy to WhoseTurn + 1
                     else:
                         self.childnodes.append(None)
         elif (self.WhoseTurn % 2) == 0:
@@ -128,7 +127,7 @@ class Matchbox:
                     if self.grid[i][j] != "x" and self.grid[i][j] != "o":
                         new_grid[i][j] = "o"
                         # print(f"flag one 1\n\nnew grid is {new_grid}")
-                        self.childnodes.append(Matchbox(new_grid, [self], [], self.beads, thingy)) #TODO change thingy to WhoseTurn + 1
+                        self.childnodes.append(Matchbox(new_grid, self, [], self.beads, thingy)) #TODO change thingy to WhoseTurn + 1
 
                     else:
                         # print(f"flag 2\n")
@@ -145,6 +144,7 @@ class Matchbox:
         current_grid = copy.deepcopy(grid)
         user_go = input(f" {grid[0]} \n {grid[1]}\n {grid[2]}\nPut your move in (row,column) format, with no space: ")
         user_go = eval(user_go)
+        print(user_go)
         if type(user_go) != type((0, 0)) or len(user_go) != 2 or type(grid[user_go[0]][user_go[1]]) != type(
                 0):  # checking if user_go is the right format
             print(f"you have put the answer in the wrong format, or used occupied cell. Please try again. ")
@@ -153,53 +153,64 @@ class Matchbox:
             return user_go
 
     def game(self):
-        if self.WhoseTurn % 1 == 0:
-            count = 0
-            for row in range(0, 3):
-                for col in range(0, 3):
-                    if self.grid[row][col] != 'x' and self.grid[row][col] != 'o':
-                        count += self.grid[row][col]      #count the total number of beads
-
-            random_x = random.randint(0, count)       #set the random number
-            break_out_flag = False
-            index = 0           #set the index of the game.
-            for row2 in range(0, 3):
-                for col2 in range(0, 3):
-                    print(f"random x is {random_x}")
-                    if type(self.grid[row2][col2]) == type(0):  # checking whether column in a row is an integer
-                        if random_x - self.grid[row2][col2] <= 0: #todo check indexing problem
-                            break_out_flag = True
-                            break
-                        else:
-                            index += 1
-                            random_x -= self.grid[row2][col2]
-                if break_out_flag:
-                    break
-                    # else:
-                    #     index += 1
-                    #     random_x -= self.grid[row2][col2]
-            placed_x = (row2, col2)
-            print(f"here we go...\nINDEX is {index},\nCURRENT game board is {self.grid}, \nCHILDNODES are {self.childnodes}")
-            self.result = self.childnodes[index].game()
-        else:
-            user_cords = self.user_go(self.grid)
-            index = ((3 * user_cords[0]) + (user_cords[1])) # calculates the index of the matchbox with the same game
-            # grid inside the childnode list.
-            self.result = self.childnodes[index].game()
         if self.checking_win()[1] == "Menace":
+            print("Menace has won")
             return True
         elif self.checking_win()[1] == "Player":
+            print("Player has won")
             return False
         elif self.checking_win()[0] is False:
-            pass
-            # if self.game(matchbox) == True:
-            #     self.parentnode.grid[placed_x[0]][placed_x[1]] += 3
-            # else:
-            #     pass
+            if self.WhoseTurn % 2 != 0:
+                count = 0
+                for row in range(0, 3):
+                    for col in range(0, 3):
+                        if self.grid[row][col] != 'x' and self.grid[row][col] != 'o':
+                            count += self.grid[row][col]      #count the total number of beads
 
+                random_x = random.randint(0, count)       #set the random number
+                break_out_flag = False
+                index = 0           #set the index of the game.
+                for row2 in range(0, 3):
+                    for col2 in range(0, 3):
+                        print(f"random x is {random_x}")
+                        if type(self.grid[row2][col2]) == type(0):  # checking whether column in a row is an integer
+                            if random_x - self.grid[row2][col2] <= 0: #todo check indexing problem
+                                break_out_flag = True
+                                break
+                            else:
+                                random_x -= self.grid[row2][col2]
+                                index += 1
+                        else:
+                            index += 1
+                            pass
+                    if break_out_flag:
+                        self.row2 = row2
+                        self.col2 = col2
+                        self.placed_x = (self.row2, self.col2)
+                        break
+                self.result = self.childnodes[index].game()
+                print(self.placed_x)
+                if self.result == True:
+                    self.parentnode.grid[self.placed_x[0]][self.placed_x[1]] += 3
+                elif self.result == False:
+                    self.parentnode.grid[self.placed_x[0]][self.placed_x[1]] -= 2
+            elif self.WhoseTurn % 2 == 0:
+                user_cords = self.user_go(self.grid)
+                index = ((3 * user_cords[0]) + (user_cords[1])) # calculates the index of the matchbox with the same game
+                # grid inside the childnode list.
+                self.result = self.childnodes[index].game()
+                if self.result == True:
+                    self.parentnode.grid[self.placed_x[0]][self.placed_x[1]] += 3
+                elif self.result == False:
+                    self.parentnode.grid[self.placed_x[0]][self.placed_x[1]] -= 2
+            if self.checking_win()[1] == "Menace":
+                return True
+            elif self.checking_win()[1] == "Player":
+                return False
 
-# matchbox = Matchbox(GRIDS, [], [], beads, WhoseTurn=WHOSE_TURN_IS_IT)
 
 matchbox = Matchbox(GRIDS, [], [], beads, WhoseTurn=WHOSE_TURN_IS_IT)
-matchbox.game()
+user_decided = int(input("please input the number of times you want to play a game please "))
+for i in range(user_decided):
+    matchbox.game()
 # print(f"\n\nParent node is {matchbox.childnodes[0].grid}, Whoseturn {matchbox.childnodes[0].WhoseTurn}\n childnode is {matchbox.childnodes[0].childnodes[2].grid}\n\n")
